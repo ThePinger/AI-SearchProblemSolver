89e5f9e87de0f1158b7d512598c740893a6db7de
Unknown <mohamedynawar@gmail.com>
2019-10-10 17:09:57 +0200

    [+] Initialized the Node class and the Operator interface.

--- src/genericsearch/Node.java
+++ src/genericsearch/Node.java
@@ -2,5 +2,43 @@ package genericsearch;
 
 public class Node 
 {
-
+	private State state;		// This variable represents the state of the given node
+	private Node parent;		// This is a reference to the parent of this node
+	private Operator operator;	// This is a reference to the operator applied to the parent node to get to this node
+	private int depth;			// This is an integer representing the depth of this node in the search tree
+	private int pathCost;		// This is an integer representing the cost of the sequence of actions required to reach this node from the root
+	
+	public Node(State state, Node parent, Operator operator, int depth, int pathCost)
+	{
+		this.state = state;
+		this.parent = parent;
+		this.operator = operator;
+		this.depth = depth;
+		this.pathCost = pathCost;
+	}
+	
+	public int getPathCost()
+	{
+		return this.pathCost;
+	}
+	
+	public int getDepth()
+	{
+		return this.depth;
+	}
+	
+	public Operator getOperator()
+	{
+		return this.operator;
+	}
+	
+	public State getState()
+	{
+		return this.state;
+	}
+	
+	public Node getParent()
+	{
+		return this.parent;
+	}
 }
--- src/genericsearch/Operator.java
+++ src/genericsearch/Operator.java
@@ -2,5 +2,10 @@ package genericsearch;
 
 public interface Operator 
 {
-
+	/**
+	 * This method applies this operator to the given state. It then returns the new state after the operator has been applied
+	 * @param oldState The state that this operator is going to be applied to
+	 * @return The state after applying this operator on the old state.
+	 */
+	public State apply(State oldState);
 }
