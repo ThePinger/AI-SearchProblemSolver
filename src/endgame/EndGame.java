6b6a854c5f71c5b5601168a7e0e610a97f7a0f92
Unknown <mohamedynawar@gmail.com>
2019-10-10 22:43:33 +0200

    [+] Initialized the EndGame class.

--- src/endgame/EndGame.java
+++ src/endgame/EndGame.java
@@ -4,5 +4,37 @@ import genericsearch.SearchProblem;
 
 public class EndGame extends SearchProblem
 {
-
+	private static int m; // an integer representing the maximum length of the grid
+	private static int n; // an integer representing the maximum width of the grid
+	private static Position thanosLocation; // Thanos' position in the grid
+	
+	public int getM()
+	{
+		return m;
+	}
+	
+	public void setM(int m)
+	{
+		EndGame.m = m;
+	}
+	
+	public int getN()
+	{
+		return n;
+	}
+	
+	public void setN(int n)
+	{
+		EndGame.n = n;
+	}
+	
+	public Position getThanosLocation()
+	{
+		return thanosLocation;
+	}
+	
+	public void setThanosLocation(Position thanosLocation)
+	{
+		EndGame.thanosLocation = thanosLocation;
+	}
 }
