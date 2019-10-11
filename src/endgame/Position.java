361641a7ff64e7ec1d61b49a58f2f2fa4a637f44
Omar Emad <omaremadsabry@gmail.com>
2019-10-11 21:02:52 +0200

    [+] Added Equals & ToString Functions in Position

--- src/endgame/Position.java
+++ src/endgame/Position.java
@@ -33,6 +33,21 @@ public class Position implements Comparable<Position>
 	}
 	
 	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 * Checks if two Position objects are identical
+	 */
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if(!(obj instanceof Position)) return false;
+
+		Position otherPosition = (Position) obj;
+		
+		return this.x == otherPosition.x && this.y == otherPosition.y;
+	}
+	
+	/*
 	 * This function compares two positions A, B. 
 	 * IF position A equal to position B returns 0	
 	 * ELSE compares according to X, Y (Not Used in the Project)
@@ -45,4 +60,9 @@ public class Position implements Comparable<Position>
 		return this.x - p.x;
 	}
 	
+	@Override
+	public String toString() 
+	{
+		return "Position: X = " + this.x + ", Y = " + this.y;
+	}
 }
