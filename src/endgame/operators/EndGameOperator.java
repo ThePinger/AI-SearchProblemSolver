aa79ee4e26903e8f65779536e07d4c50b4f83154
Mohamed Nawar <mohamedynawar@gmail.com>
2019-10-11 17:21:47 +0200

    [+] Added the EndGameOperator class and added /bin directory to .gitignore.

--- src/endgame/operators/EndGameOperator.java
+++ src/endgame/operators/EndGameOperator.java
@@ -1,8 +1,22 @@
 package endgame.operators;
 
+import java.util.TreeSet;
+
+import endgame.Position;
 import genericsearch.Operator;
+import genericsearch.State;
 
 public abstract class EndGameOperator implements Operator
 {
-
+	public boolean isValidMove(State newState)
+	{
+		// TODO Shady
+		return false;
+	}
+	
+	public int calculateDamage(Position ironManPosition, TreeSet<Position> aliveWarriors)
+	{
+		// TODO Shady
+		return 0;
+	}
 }
