4e8685320e12184823d51690d78a5b564560c41d
mohamedynawar <mohamedynawar@gmail.com>
2019-10-11 23:12:41 +0200

    [+] Initialized the AuxiliaryQueue class.
    The purpose of this class is to make the queuing process easier in the Search procedure.

---
+++ src/genericsearch/AuxiliaryQueue.java
@@ -0,0 +1,31 @@
+package genericsearch;
+
+import java.util.Queue;
+
+public class AuxiliaryQueue
+{
+	private Object auxQueue;
+	private QINGFunction qingFunction;
+	
+	public AuxiliaryQueue(QINGFunction Q)
+	{
+		// TODO 7alawa: create a data structure according to the qing function
+	}
+	
+	public void insertNewNodes(Queue<Node> newNodes)
+	{
+		// TODO 7alawa: adds the nodes in the queue to the AuxQueue
+	}
+	
+	public boolean isEmpty()
+	{
+		// TODO 7alawa: returns if it is empty
+		return false;
+	}
+	
+	public Node removeFront()
+	{
+		// TODO 7alawa: removes the front of the aux queue
+		return null;
+	}
+}
