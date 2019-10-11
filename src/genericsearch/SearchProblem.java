231ce67af260e4cec526e24a257caf5bd61e7069
ShadyTantawy <ShadyTantawy@protonmail.com>
2019-10-11 17:36:16 +0200

    Initialized SearchProblem Class
    [+] genericOperators
    [+] initialState
    [+] stateSpace
    [+] goalTest (abstract)
    [+] pathCost (abstract)
    [+] createInitialState (abstract)

--- src/genericsearch/SearchProblem.java
+++ src/genericsearch/SearchProblem.java
@@ -1,6 +1,45 @@
 package genericsearch;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public abstract class SearchProblem 
 {
+	private ArrayList <Operator> genericOperators;
+	private State initialState;
+	private HashSet <State> stateSpace;
+	
+	public ArrayList <Operator> getGenericOperators()
+	{
+		return this.genericOperators;
+	}
+	
+	public void setGenericOperators(ArrayList <Operator> genericOperators)
+	{
+		this.genericOperators = genericOperators;
+	}
+
+	public State getInitialState()
+	{
+		return this.initialState;
+	}
+
+	public void setInitialState(State initialState)
+	{
+		this.initialState = initialState;
+	}
+
+	public HashSet <State> getStateSpace()
+	{
+		return this.stateSpace;
+	}
+	
+	public abstract boolean goalTest(Node node);
+	
+	public abstract int pathCost(State state);
+	
+	public abstract State createInitialState();
 
 }
