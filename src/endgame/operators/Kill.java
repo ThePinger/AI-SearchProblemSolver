1b8b7db562499c5652640a07ed9d5fa850e2ac78
Omar Emad <omaremadsabry@gmail.com>
2019-10-12 22:14:01 +0200

    [+] Added Up, Down & Kill Operators

--- src/endgame/operators/Kill.java
+++ src/endgame/operators/Kill.java
@@ -1,6 +1,44 @@
 package endgame.operators;
 
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+import endgame.EndGameState;
+import endgame.Position;
+import genericsearch.State;
+
 public class Kill extends EndGameOperator 
 {
-
+	/**
+	 * This method applies the Kill operator which kills all warriors in adjacent cells
+	 * where IronMan damage increases by 2 for each killed warrior
+	 * @param oldState
+	 * @return New State if a new state is valid otherwise returns null
+	 */
+	@Override
+	public State apply(State oldState) 
+	{
+		EndGameState curState = (EndGameState) oldState;
+		Position ironManPosition = curState.getIronManPosition();
+		TreeSet<Position> aliveWarriors = curState.getAliveWarriors();
+		
+		ArrayList<Position> adjacentCells = super.getAdjacentCells(ironManPosition);
+		int newDamage = curState.getDamage();
+		for(Position p : adjacentCells)
+		{
+			if(aliveWarriors.contains(p))
+			{
+				newDamage += 2;
+				aliveWarriors.remove(p);
+			}
+		}
+		
+		newDamage += super.calculateDamage(ironManPosition, aliveWarriors);
+		
+		EndGameState newState = new EndGameState(ironManPosition, newDamage, curState.getUncollectedStones(), aliveWarriors, curState.isThanosAlive());
+		
+		if(super.isValidMove(newState) && newDamage < 100)
+			return newState;
+		return null;
+	}
 }
--- src/endgame/operators/Up.java
+++ src/endgame/operators/Up.java
@@ -1,6 +1,33 @@
 package endgame.operators;
 
+import java.util.TreeSet;
+
+import endgame.EndGameState;
+import endgame.Position;
+import genericsearch.State;
+
 public class Up extends EndGameOperator 
 {
 
+	/**
+	 * This method applies the UP operator which moves IronMan up the grid
+	 * by decrementing X position by 1
+	 * @param oldState
+	 * @return New State if a new state is valid otherwise returns null
+	 */
+	@Override
+	public State apply(State oldState) 
+	{
+		EndGameState curState = (EndGameState) oldState;
+		Position newIronManPosition = new Position(curState.getIronManPosition().getX() - 1, curState.getIronManPosition().getY());
+		TreeSet<Position> aliveWarriors = curState.getAliveWarriors();
+		int newDamage = curState.getDamage() + super.calculateDamage(newIronManPosition, aliveWarriors);
+		
+		EndGameState newState = new EndGameState(newIronManPosition, newDamage, curState.getUncollectedStones(), aliveWarriors, curState.isThanosAlive());
+		
+		if(super.isValidMove(newState) && newDamage < 100)
+			return newState;
+		return null;
+	}
+	
 }
