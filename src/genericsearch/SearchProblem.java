f1722e38f6ee492e75efcd72c95d68799a4c3509
ShadyTantawy <ShadyTantawy@protonmail.com>
2019-10-13 19:34:18 +0200

    [*] Edited generalSearch Method

--- src/genericsearch/SearchProblem.java
+++ src/genericsearch/SearchProblem.java
@@ -44,8 +44,8 @@ public abstract class SearchProblem
 	
 	public Node generalSearch(QINGFunction q, int maximumDepth)
 	{
-		this.setInitialState(this.createInitialState());
-		Node rootNode = new Node(this.getInitialState(), null, null, maximumDepth, 0);
+		this.initialState = this.createInitialState();
+		Node rootNode = new Node(this.initialState, null, null, 0, 0);
 		Queue <Node> queue = new LinkedList<Node>();
 		queue.add(rootNode);
 		AuxiliaryQueue auxQ = new AuxiliaryQueue(q);
@@ -53,10 +53,10 @@ public abstract class SearchProblem
 		while( !(auxQ.isEmpty()) )
 		{
 			Node node = auxQ.removeFront();
-			if (node.getDepth() == maximumDepth + 1)
-				break;
 			if(this.goalTest(node))
 				return node;
+			if (node.getDepth() == maximumDepth)
+				continue;
 			auxQ.insertNewNodes(this.expand(node));
 		}
 		return null;
