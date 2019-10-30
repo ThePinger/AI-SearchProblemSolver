138eb96de9d4f7996cf61ea08feae5123c66f210
Omar Emad <omaremadsabry@gmail.com>
2019-10-30 13:35:56 +0200

    [*] Updated Node compareTo Function

--- src/genericsearch/Node.java
+++ src/genericsearch/Node.java
@@ -9,7 +9,8 @@ public class Node implements Comparable<Node>
 	private int pathCost;	// This is an integer representing the cost of the sequence of actions required to reach this node from the root
 	private double expectedCostToGoal; // This is a double representing the expected cost to goal, it is set to -1 if the Qing function used does not use a heuristic function
 	private QINGFunction qingFunction; // This represents the qing function being used while this node was created 
-	
+    private final double EPS = 1e-9; // This variable used to compare doubles in case to determine if they are equal
+
 	public Node(State state, Node parentNode, Operator appliedOperator, int depth, int pathCost, QINGFunction Q)
 	{
 		this.state = state;
@@ -62,11 +63,16 @@ public class Node implements Comparable<Node>
 	}
 
 	/**
-	 * This method returns a negative number if this node has lower cost than Node o, returns 0 if they have identical costs,
-	 * and return a positive number if Node o has a lower cost.
+	 * This method compares according to the Node QingFunction
+	 * InCase of the UCS QingFunction it compares the Path Cost
+	 * Otherwise it compares according to the expected cost
 	 */
 	public int compareTo(Node o)
 	{
-		return this.pathCost - o.pathCost;
+		if(this.qingFunction == QINGFunction.UCS)
+			return this.pathCost - o.pathCost;
+		if(Math.abs(this.expectedCostToGoal - o.expectedCostToGoal) > this.EPS)
+			return this.expectedCostToGoal > o.expectedCostToGoal ? 1 : -1;
+		return 0;
 	}
 }
