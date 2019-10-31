a89d698c3b3df43709f93ec92ef55d54e2484dad
Omar Emad <omaremadsabry@gmail.com>
2019-10-31 15:31:21 +0200

    [*] Updated main method

--- src/Main.java
+++ src/Main.java
@@ -50,7 +50,7 @@ public class Main
 	
 	public static void main(String[] args) 
 	{
-		solve("5,5;1,2;3,1;0,2,1,1,2,1,2,2,4,0,4,1;0,3,3,0,3,2,3,4,4,3", "BF", false);
+		System.out.println(solve("5,5;1,2;3,1;0,2,1,1,2,1,2,2,4,0,4,1;0,3,3,0,3,2,3,4,4,3", "BF", false));
 	}
 
 }
